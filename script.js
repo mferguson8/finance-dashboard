ccb5c7589fbe1d51be1a78f1227fd5bd4a1fb30a
Michael Ferguson <mferg3838@gmail.com>
2023-07-12 23:32:30 -0400

    added new dashboard option

--- script.js
+++ script.js
@@ -1,37 +1,62 @@
 document.addEventListener("DOMContentLoaded", function () {
+  // Initialize the total income, expenses, net income, and remaining budget
+  let totalIncome = 0;
+  let totalExpenses = 0;
+  let netIncome = 0;
+  let remainingBudget = 0;
+
   // Calculate and display the total income, expenses, and net income
-  // Replace these values with your actual logic to fetch and calculate the data
-  const totalIncome = 5000;
-  const totalExpenses = 3000;
-  const netIncome = totalIncome - totalExpenses;
+  function calculateSummary() {
+    document.getElementById("total-income").textContent =
+      "$" + totalIncome.toFixed(2);
+    document.getElementById("total-expenses").textContent =
+      "$" + totalExpenses.toFixed(2);
+    document.getElementById("net-income").textContent =
+      "$" + netIncome.toFixed(2);
+  }
 
-  document.getElementById("total-income").textContent =
-    "$" + totalIncome.toFixed(2);
-  document.getElementById("total-expenses").textContent =
-    "$" + totalExpenses.toFixed(2);
-  document.getElementById("net-income").textContent =
-    "$" + netIncome.toFixed(2);
+  // Update the remaining budget
+  function updateRemainingBudget() {
+    const budgetInput = document.getElementById("budget-input");
+    remainingBudget = budgetInput.value - totalExpenses;
+    document.getElementById("remaining-budget").textContent =
+      "$" + remainingBudget.toFixed(2);
+  }
 
-  // Update the remaining budget when the user clicks the "Update" button
+  // Update the summary and remaining budget when the user clicks the "Update" button
   document
     .getElementById("update-budget")
     .addEventListener("click", function () {
       const budgetInput = document.getElementById("budget-input");
-      const remainingBudget = budgetInput.value - totalExpenses;
-      document.getElementById("remaining-budget").textContent =
-        "$" + remainingBudget.toFixed(2);
+      totalIncome = Number(budgetInput.value);
+      netIncome = totalIncome - totalExpenses;
+      updateRemainingBudget();
+      calculateSummary();
     });
 
   // Add a financial goal to the list when the user clicks the "Add" button
   document.getElementById("add-goal").addEventListener("click", function () {
     const goalInput = document.getElementById("goal-input");
-    const goal = goalInput.value;
+    const goal = Number(goalInput.value);
     const goalsList = document.getElementById("goals-list");
     const goalItem = document.createElement("li");
     goalItem.textContent = "$" + goal.toFixed(2);
     goalsList.appendChild(goalItem);
     goalInput.value = "";
   });
+
+  // Add a purchase and update the summary when the user clicks the "Add" button
+  document
+    .getElementById("add-purchase-btn")
+    .addEventListener("click", function () {
+      const purchaseAmountInput = document.getElementById("purchase-amount");
+      const purchaseAmount = Number(purchaseAmountInput.value);
+      totalExpenses += purchaseAmount;
+      netIncome = totalIncome - totalExpenses;
+      updateRemainingBudget();
+      calculateSummary();
+      purchaseAmountInput.value = "";
+    });
 
   // Create a sample spending habits chart using Chart.js
   // Replace this code with your actual chart logic and data
